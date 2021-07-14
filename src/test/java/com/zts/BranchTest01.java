b184a4277e791a24dccd980f8a262e7a5e0d22ed
zts <1292933220@qq.com>
2021-07-14 16:44:56 +0800

    left01分支初始化操作-----

---
+++ src/test/java/com/zts/BranchTest01.java
@@ -0,0 +1,13 @@
+package com.zts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BranchTest01 {
+
+    void BranchTest01(){
+        System.out.println("left01分支-----被创建");
+    }
+
+}
